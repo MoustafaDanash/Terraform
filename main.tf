6ac13ffe3b017380b18b79082382ae8780b2e27f
Moustafa <moustafadanash@ymail.com>
2023-12-04 16:28:51 +0200

    first one

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "admin"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-06d4b7182ac3480fa"
+  instance_type = lookup(var.instance_typo, terraform.workspace)
+}
+
+variable "instance_typo" {
+  type = map(any)
+  default = { "dev" = "t2.micro",
+    "staging" = "t2.medium",
+  "prod" = "t2.large" }
+}
