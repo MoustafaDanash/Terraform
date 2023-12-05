279eaae4cd2ae1382a195499826180c1418fc9fb
MoustafaDanash <123922676+MoustafaDanash@users.noreply.github.com>
2023-12-05 14:46:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,5 @@ provider "aws" {
 
 resource "aws_instance" "myec2" {
   ami           = "ami-06d4b7182ac3480fa"
-  instance_type = lookup(var.instance_typo, terraform.workspace)
-}
-
-variable "instance_typo" {
-  type = map(any)
-  default = { "dev" = "t2.micro",
-    "staging" = "t2.medium",
-  "prod" = "t2.large" }
+  instance_type = "t2.micro"
 }
